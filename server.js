b5afcdd49a199906cbec116bb48363d24e73f559
raul854 <raul.barbosa@estudante.ifms.edu.br>
2026-09-25 16:52:01 -0400

    feat: conecta middlewares de erro ao servidor

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usuariosRoutes = require('./routes/usuarios.routes');
+const { naoEncontrado, tratadorDeErros } = require('./middlewares/erros');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/usuarios', usuariosRoutes);
+
+app.use(naoEncontrado);
+app.use(tratadorDeErros);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
